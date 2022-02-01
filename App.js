65fbc4ce228a9792ffcb0925dbe3a351254efd0d
nateeither <35027778+nateeither@users.noreply.github.com>
2022-02-01 22:10:28 +0700

    configure ios

--- App.js
+++ App.js
@@ -2,7 +2,59 @@ import React from 'react'
 import { Text, View, Button, StyleSheet } from 'react-native'
 import { LocalNotification } from './LocalPushController'
 import RemotePushController from './RemotePushController'
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+
+
 const App = () => {
+  const [permissions, setPermissions] = useState({});
+
+  useEffect(() => {
+    const type = 'notification';
+
+    PushNotificationIOS.requestPermissions({
+      alert: true,
+      badge: true,
+      sound: true,
+      critical: true,
+    }).then(
+      (data) => {
+        console.log('PushNotificationIOS.requestPermissions', data);
+      },
+      (data) => {
+        console.log('PushNotificationIOS.requestPermissions failed', data);
+      },
+    );
+
+
+    PushNotificationIOS.addEventListener(type, onRemoteNotification);
+    return () => {
+      PushNotificationIOS.removeEventListener(type);
+    };
+    
+  });
+
+  const onRemoteNotification = (notification) => {
+    console.log('Notif ios ', notification)
+    const isClicked = notification.getData().userInteraction === 1;
+
+    if (isClicked) {
+      // Navigate user to another screen
+    } else {
+      // Do something else with push notification
+    }
+  };
+
+  const sendLocalNotificationWithSound = () => {
+    PushNotificationIOS.addNotificationRequest({
+      id: 'notificationWithSound',
+      title: 'Sample Title IOS',
+      subtitle: 'Sample Subtitle',
+      body: 'Sample local notification with custom sound',
+      sound: 'customSound.wav',
+      badge: 1,
+    });
+  };
+
   const handleButtonPress = () => {
     LocalNotification()
   }
@@ -11,7 +63,8 @@ const App = () => {
     <View style={styles.container}>
       <Text>Press a button to trigger the notification</Text>
       <View style={{ marginTop: 20 }}>
-        <Button title={'Local Push Notification'} onPress={handleButtonPress} />
+        <Button title={'Local Push Notification Android'} onPress={handleButtonPress} />
+        <Button title={'Local Push Notification Ios'} onPress={sendLocalNotificationWithSound} />
       </View>
       <View>
         <RemotePushController />
